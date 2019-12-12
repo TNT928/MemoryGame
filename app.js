9ef6409dd6c2a61973b3dad58644f6791074ac47
john <wayaway.jtm@gmail.com>
2019-12-11 21:28:35 -0500

    added ShuffleCards function

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 let cards = document.querySelectorAll('.card');
 const CARD_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-let shuffledCardValues = shuffle(CARD_VALUES);
+function ShuffleCards() {
+  let shuffledCardValues = shuffle(CARD_VALUES);
 
-for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < cards.length; i++) {
     cards[i].innerHTML += shuffledCardValues[i];
+  }
 }
 
 // Fisher-Yates Shuffle
 // source: https://bost.ocks.org/mike/shuffle/
 function shuffle(array) {
-    var m = array.length, t, i;
-  
-    // While there remain elements to shuffle…
-    while (m) {
-  
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * m--);
-  
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
-    }
-  
-    return array;
+  var m = array.length,
+    t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
   }
+
+  return array;
+}
