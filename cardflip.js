9afd2000cfc4e4e4d8d6311013ad098d235d8715
john <wayaway.jtm@gmail.com>
2019-12-18 18:21:57 -0500

    formatted code

--- cardflip.js
+++ cardflip.js
@@ -1,26 +1,26 @@
-let isMatch = (flipCards)=>{
-  let src1 =  flipCards[0].getAttribute('src');
-  let src2 =  flipCards[1].getAttribute('src');
+let isMatch = (flipCards) => {
+  let src1 = flipCards[0].getAttribute('src');
+  let src2 = flipCards[1].getAttribute('src');
 
-  if(src1 === src2){
+  if (src1 === src2) {
     return true;
   } else {
-      return false;
-    }
+    return false;
+  }
 
 }
 
 let mom = document.querySelectorAll('.card');
 let child = document.querySelectorAll('.card_back')
 
-if(isMatch(child)){
-    mom[0].removeChild(child[0])
-    mom[1].removeChild(child[1])
-   
+if (isMatch(child)) {
+  mom[0].removeChild(child[0])
+  mom[1].removeChild(child[1])
+
 }
 
-let removeCard = ()=> {
-	mom[0].removeChild(child[0])
+let removeCard = () => {
+  mom[0].removeChild(child[0])
 
 }
 
@@ -32,7 +32,3 @@ let cardToRemove = child[0];
 
 
 // isMatch(flipCards)
-
-
-
-
