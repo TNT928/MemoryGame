dc708f9140484c26c26b084e3c5152e147164168
Jordan Gaza <Gaza>
2019-12-19 16:35:01 -0500

    Adding possible second function, checkForMatch

--- dontmatch.js
+++ dontmatch.js
@@ -5,20 +5,29 @@ let firstCard, secondCard;
 
 function flipCard() {
     this.classList.add('flip');
+    if (this === firstCard) return;
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+    secondCard = this;
+    checkForMatch();
 }
+
+function checkForMatch() {
+    
+  }
 
 //possible other matching function?? Come back to this
-function reFlip(flippedCards) {
-  let flippedCards = document.querySelectorAll(".is-flipped");
-  if (backgroundImage) {
-    this.classList.toggle("match");
-  } else {
-    this.classList.toggle("noMatch");
-  }
-}
 
-flippedCards.forEach(card => card.addEventListener("click", flipCard));
+// function reFlip(flippedCards) {
+//   let flippedCards = document.querySelectorAll(".is-flipped");
+//   if (backgroundImage) {
+//     this.classList.toggle("match");
+//   } else {
+//     this.classList.toggle("noMatch");
+//   }
+// }
+
+// flippedCards.forEach(card => card.addEventListener("click", flipCard));
